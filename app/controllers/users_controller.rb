b46e95ec9b1a6f70c6b6ad8b59be8e2521e27831
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2022-12-26 23:59:26 +0000

    [Finish]Application-task7d

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+  
+      @reviews = Review.all
+    @all_rating = '総合評価'
+    @rating1 = '評価1'
+    @rating2 = '評価2'
+    @rating3 = '評価3'
+    @rating4 = '評価4'
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
